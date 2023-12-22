b89276ee83ed8e3e9f306146891976e7c5bed195
MH <noodelcho@i-way.co.kr>
2023-12-22 13:14:20 +0900

    v0.1.5

---
+++ README.md
@@ -0,0 +1,17 @@
+# buffer_batch
+
+---
+
+#### QGIS 플러그인
+
+많은 양의 shape 레이어 buffer 처리를 도와주는 플러그인
+
+---
+##### latest_version : 0.1.5
+* 개발 QGIS 버전 :  3.22.16
+
+##### change log
+* 배치모드, 싱글모드
+
+
+
--- buffer_batch_dialog.py
+++ buffer_batch_dialog.py
@@ -48,88 +48,140 @@ class buffer_batchDialog(QtWidgets.QDialog, FORM_CLASS):
         # http://qt-project.org/doc/qt-4.8/designer-using-a-ui-file.html
         # #widgets-and-dialogs-with-auto-connect
         self.setupUi(self)
+        self.init_ui()
         self.init()
 
-    def init(self):
+    def init_ui(self):
         self.progressBar.setVisible(False)
+        self.rdo_batch.setChecked(True)
+        self.lb_input.setText(self.tr("입력 경로"))
+        self.lb_output.setText(self.tr("결과 경로"))
+        self.lb_dist.setText(self.tr("거리값"))
+        self.txt_input.setText("") # 초기화
+        self.txt_output.setText("") # 초기화
+
+
+
+    def init(self):
         self.btn_input.clicked.connect(lambda : self.open_folder(self.txt_input))
         self.txt_dist.setText("3")
-        # self.btn_output.clicked.connect()
+        self.btn_output.clicked.connect(lambda: self.save_folder(self.txt_output))
         self.btn_run.clicked.connect(self.buffer_run)
+        self.rdo_batch.clicked.connect(self.change_init)
+        self.rdo_single.clicked.connect(self.change_init)
+
+    def change_init(self):
+        if self.rdo_batch.isChecked() ==  True:
+            self.lb_input.setText(self.tr("입력 경로"))
+            self.lb_output.setText(self.tr("결과 경로"))
+            self.txt_input.setText("")  # 초기화
+            self.txt_output.setText("")  # 초기화
+            self.btn_output.setEnabled(False)
+        else:
+            self.lb_input.setText(self.tr("입력 파일"))
+            self.lb_output.setText(self.tr("결과 파일"))
+            self.txt_input.setText("")  # 초기화
+            self.txt_output.setText("")  # 초기화
+            self.btn_output.setEnabled(True)
 
 
+
+    # region [input 경로 선택]
     def open_folder(self, text):
         self.txt_result.setText("")
+        # region [배치모드]
+        if self.rdo_batch.isChecked() ==  True:
+            try:
+                # folder = QFileDialog.getExistingDirectory(self, "Select folder")
+                # text.setText(folder)
+
+                file_dialog = QFileDialog()
+                file_dialog.setFileMode(QFileDialog.DirectoryOnly)
+                file_dialog.setOption(QFileDialog.DontUseNativeDialog, True)
+                # file_view = file_dialog.findChild(QListView, 'listView')
+                # to make it possible to select multiple directories:
+                # if file_view:
+                #    file_view.setSelectionMode(QAbstractItemView.MultiSelection)
+                f_tree_view = file_dialog.findChild(QTreeView)
+                if f_tree_view:
+                    f_tree_view.setSelectionMode(QAbstractItemView.MultiSelection)
+
+                if file_dialog.exec():
+                    folders = file_dialog.selectedFiles()
+
+                    check_list=list()
+                    for i in folders:
+                        print (i,folders[0])
+                        check_list.append(folders[0] in i)
+
+                    if len(folders) ==(check_list.count(True)):
+                        folders1 = folders[1:]
+                        print (folders1)
+                    else:
+                        folders1= folders
+                        print (folders1)
+                    text.setText(str(folders1).replace("[","").replace("]","").replace("'",""))
+                    QMessageBox.information(self,"선택폴더","선택된 폴더 : {0}".format(str(folders1)))
+
+
+            except Exception as e:
+                print (e.__str__())
+                QMessageBox.warning(self, '오류', e.__str__())
+        # endregion
+        # region [싱글모드 - 단일파일모드]
+        elif self.rdo_batch.isChecked() ==  False:
+            try:
+                fileName=QFileDialog.getOpenFileName(self,"Select Shape File",r"","Shapefiles(*.shp)")
+                text.setText(fileName[0])
+            except Exception as e:
+                print(e.__str__())
+                QMessageBox.warning(self, '오류', e.__str__())
+        # endregion
+    # endregion
+
+    def save_folder(self,text):
         try:
-            # folder = QFileDialog.getExistingDirectory(self, "Select folder")
-            # text.setText(folder)
-
-            file_dialog = QFileDialog()
-            file_dialog.setFileMode(QFileDialog.DirectoryOnly)
-            file_dialog.setOption(QFileDialog.DontUseNativeDialog, True)
-            # file_view = file_dialog.findChild(QListView, 'listView')
-            # to make it possible to select multiple directories:
-            # if file_view:
-            #    file_view.setSelectionMode(QAbstractItemView.MultiSelection)
-            f_tree_view = file_dialog.findChild(QTreeView)
-            if f_tree_view:
-                f_tree_view.setSelectionMode(QAbstractItemView.MultiSelection)
-
-            if file_dialog.exec():
-                folders = file_dialog.selectedFiles()
-                
-                check_list=list()
-                for i in folders:
-                    print (i,folders[0])
-                    check_list.append(folders[0] in i)
-
-                if len(folders) ==(check_list.count(True)):
-                    folders1 = folders[1:]
-                    print (folders1)
-                else:
-                    folders1= folders
-                    print (folders1)
-                text.setText(str(folders1).replace("[","").replace("]","").replace("'",""))
-                QMessageBox.information(self,"선택폴더","선택된 폴더 : {0}".format(str(folders1)))
-
-
+            folder=QFileDialog.getExistingDirectory(None,"Select folder")
+            text.setText(folder)
         except Exception as e:
-            print (e.__str__())
+            print(e.__str__())
+            QMessageBox.warning(self, '오류', e.__str__())
+
 
     # batch 수행
     def buffer_run(self):
-        inputPath = self.txt_input.text()
+
         QApplication.processEvents()
-        print (inputPath.split(","))
+        if self.rdo_batch.isChecked() == True:
+            inputPath = self.txt_input.text()
+            for folder in inputPath.split(","):
+                i_folder=folder.strip()
+                outputPath = os.path.dirname(i_folder) + "/{0}_output/".format(os.path.basename(i_folder))
+                print (outputPath)
+                self.txt_output.setText(outputPath)
+                if os.path.exists(outputPath) != True:
+                    os.mkdir(outputPath)
 
-        for folder in inputPath.split(","):
-            i_folder=folder.strip()
-            outputPath = os.path.dirname(i_folder) + "/{0}_output/".format(os.path.basename(i_folder))
-            print (outputPath)
-            self.txt_output.setText(outputPath)
-            if os.path.exists(outputPath) != True:
-                os.mkdir(outputPath)
+                for layer in (os.listdir(i_folder)):
+                    if os.path.splitext(layer)[1] == ".shp":
+                        layer_1 = i_folder + "/" + layer
+                        output = outputPath + layer.replace(".shp", "_output.shp")
+                        self.txt_result.setText("{0}\n수행중".format(output))
 
-            for layer in (os.listdir(i_folder)):
-                if os.path.splitext(layer)[1] == ".shp":
-                    layer_1 = i_folder + "/" + layer
-                    output = outputPath + layer.replace(".shp", "_output.shp")
-                    self.txt_result.setText("{0}\n수행중".format(output))
+                        parameter = {
+                            'INPUT': layer_1,
+                            'DISSOLVE': False,
+                            'DISTANCE': float(self.txt_dist.text()),
+                            'END_CAP_STYLE': 0,
+                            'JOIN_STYLE': 0,
+                            'MITER_LIMIT': 2,
+                            'SEGMENTS': 5,
+                            'OUTPUT': output
+                        }
 
-                    parameter = {
-                        'INPUT': layer_1,
-                        'DISSOLVE': False,
-                        'DISTANCE': float(self.txt_dist.text()),
-                        'END_CAP_STYLE': 0,
-                        'JOIN_STYLE': 0,
-                        'MITER_LIMIT': 2,
-                        'SEGMENTS': 5,
-                        'OUTPUT': output
-                    }
-
-                    try:
-                        result = processing.run('qgis:buffer', parameter)
-                        self.txt_result.setText("{0}\n생성 완료".format(output))
+                        try:
+                            result = processing.run('qgis:buffer', parameter)
+                            self.txt_result.setText("{0}\n생성 완료".format(output))
 
                         # # cpg 파일 생성 - EUC-KR
                         # if os.path.splitext(layer)[1] == ".cpg":
@@ -138,10 +190,35 @@ class buffer_batchDialog(QtWidgets.QDialog, FORM_CLASS):
                         #     if os.path.dirname(output).replace("shp","cpg") != True:
                         #         shutil.copy(cpg_file,os.path.dirname(output).replace("shp","cpg"))
 
-                    except Exception as e:
-                        print (e.__str__())
-                        self.txt_result.setText("")
-                        QMessageBox.warning(self, '오류', e.__str__())
+                        except Exception as e:
+                            print (e.__str__())
+                            self.txt_result.setText("")
+                            QMessageBox.warning(self, '오류', e.__str__())
+        else:
+            input = self.txt_input.text()
+            outputPath = self.txt_output.text() + "/{0}_output/".format(os.path.basename(os.path.dirname(input)))
+            if os.path.exists(outputPath) != True:
+                os.mkdir(outputPath)
+            output = outputPath + os.path.basename(input)
+            parameter = {
+                'INPUT': input,
+                'DISSOLVE': False,
+                'DISTANCE': float(self.txt_dist.text()),
+                'END_CAP_STYLE': 0,
+                'JOIN_STYLE': 0,
+                'MITER_LIMIT': 2,
+                'SEGMENTS': 5,
+                'OUTPUT': output
+            }
+
+            try:
+                result = processing.run('qgis:buffer', parameter)
+                self.txt_result.setText("{0}\n생성 완료".format(output))
+
+            except Exception as e:
+                print(e.__str__())
+                self.txt_result.setText("")
+                QMessageBox.warning(self, '오류', e.__str__())
 
 
 
